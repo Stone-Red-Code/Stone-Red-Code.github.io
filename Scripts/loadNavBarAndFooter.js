13d0a698572b7c71b8972d8fa81a95531180b766
Stone-Red-Code <56473591+StoneRed5@users.noreply.github.com>
2021-01-07 16:21:47 +0100

    Added message when JavaScript disabled

--- Scripts/loadNavBarAndFooter.js
+++ Scripts/loadNavBarAndFooter.js
@@ -1,13 +1,12 @@
-$.get("footer.html", function (data) {
-    $("#footer-placeholder").replaceWith(data);
+$.get("header.html", function (data) {
+    $("#header-placeholder").replaceWith(data);
+    document.getElementById('header-title').innerHTML = document.querySelector('title').textContent;
 });
 
 $.get("nav.html", function (data) {
     $("#nav-placeholder").replaceWith(data);
 });
 
-$.get("header.html", function (data) {
-    $("#header-placeholder").replaceWith(data);
-    document.getElementById('header-title').innerHTML = document.querySelector('title').textContent;
+$.get("footer.html", function (data) {
+    $("#footer-placeholder").replaceWith(data);
 });
-
